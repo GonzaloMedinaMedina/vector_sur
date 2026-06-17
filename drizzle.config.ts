b38628ea5b769add3da4e47401e07574b7a0d50b
Gonzalo Medina <gmedina@surcontrol.com>
2026-06-18 00:42:24 +0200

    Updating env property names

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
 import type { Config } from 'drizzle-kit'
 import * as dotenv from 'dotenv'
-dotenv.config()
+dotenv.config({ path: ".env" });
 
-const isLocal = process.env.DATABASE_URL?.startsWith('file:')
+const isLocal = process.env.DB_VECTOR_SUR_TURSO_DATABASE_URL?.startsWith('file:')
 
 export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: isLocal ? 'sqlite' : 'turso',
   dbCredentials: isLocal
-    ? { url: process.env.DATABASE_URL! }
-    : { url: process.env.DATABASE_URL!, authToken: process.env.TURSO_AUTH_TOKEN! },
+    ? { url: process.env.DB_VECTOR_SUR_TURSO_DATABASE_URL! }
+    : { url: process.env.DB_VECTOR_SUR_TURSO_DATABASE_URL!, authToken: process.env.DB_VECTOR_SUR_TURSO_AUTH_TOKEN! },
 } satisfies Config
